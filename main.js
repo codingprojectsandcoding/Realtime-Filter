064f3f79795b30be38d1fa3007b7f52e6a29ca89
codingprojectsandcoding <95212367+codingprojectsandcoding@users.noreply.github.com>
2022-11-25 16:08:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+noseX = 0;
+noseY = 0;
 function setup() {
 canvas = createCanvas(300, 300);
 canvas.center();
@@ -12,6 +14,9 @@ save("my_filter_image.png");
 }
 function draw() {
 image(video, 0, 0, 300, 300);
+fill(255, 0, 0);
+stroke(255, 0, 0);
+circle(noseX, noseY, 20);
 }
 function modelLoaded() {
 console.log("PoseNet is Initialised");
@@ -21,5 +26,7 @@ if (results.length>0) {
 console.log(results);
 console.log("nose x = " + results[0].pose.nose.x);
 console.log("nose y = " + results[0].pose.nose.y);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
 }
 }
