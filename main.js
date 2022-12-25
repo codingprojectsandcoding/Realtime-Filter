53c79d598c47d27f67fd36bfa9683076d498477d
codingprojectsandcoding <95212367+codingprojectsandcoding@users.noreply.github.com>
2022-12-25 17:54:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ clown_nose = loadImage("https://i.postimg.cc/yNtbx2WF/clown.jpg");
 function setup() {
 canvas = createCanvas(300, 300);
 canvas.center();
-video = createCapture(VIDEO);
-video.size(300, 300);
 video.hide();
 poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
